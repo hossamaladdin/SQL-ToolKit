a99ec5cf93345ad3af029c4bda466a75312df222
hossam.alaa <hossam.alaaddin@gmail.com>
2025-09-16 15:21:22 +0300

    fixing certreq

--- By Hossam/PowerShell/Configure_SSL.ps1
+++ By Hossam/PowerShell/Configure_SSL.ps1
@@ -1,15 +1,21 @@
-#create CSR
+#create CSR for LocalMachine
 $CN = "$($env:COMPUTERNAME).$((Get-WmiObject Win32_ComputerSystem).Domain)"
 $infContent = @"
+[Version]
+Signature="$Windows NT$"
+
 [NewRequest]
 Subject = "CN=$CN , O=Mobily, OU=Information Technology, L=Riyadh, S=Riyadh, C=SA"
 KeyLength = 2048
 Exportable = TRUE
+
+[RequestAttributes]
+CertificateTemplate = "WebServer"
 "@
 $infPath = [IO.Path]::Combine([Environment]::GetFolderPath("MyDocuments"), "$env:COMPUTERNAME.inf")
 $ReqPath = [IO.Path]::Combine([Environment]::GetFolderPath("MyDocuments"), "$env:COMPUTERNAME.csr")
 $infContent | Out-File -FilePath $infPath -Encoding ASCII
-certreq -new $infPath $ReqPath
+certreq -new -machine $infPath $ReqPath
 Write-Output "CSR saved to: $ReqPath"
 
 ##################################################################################################
@@ -21,14 +27,13 @@ certutil -addstore Root <rootCAfile>
 certutil -addstore CA <intermediateCAfile>
 certreq -accept <Certfile>
 
-#if didnt work, import then fix
-Import-Certificate -CertStoreLocation Cert:\LocalMachine\My -FilePath <Certfile>
-Get-ChildItem Cert:\LocalMachine\My | Format-List Subject, Thumbprint, HasPrivateKey
-certutil -repairstore my <Thumbprint>
-
 #if still didnt work, export and re-import
 $cert = Get-ChildItem Cert:\CurrentUser\My | Where-Object { $_.Subject -like "*O=Mobily*" }
 $cert | Export-PfxCertificate -FilePath "$env:USERPROFILE\Documents\MobilyCert.pfx" -Password (ConvertTo-SecureString -String "1234" -Force -AsPlainText)
 
-
 Import-PfxCertificate -FilePath "$env:USERPROFILE\Documents\MobilyCert.pfx" -CertStoreLocation Cert:\LocalMachine\My -Password (ConvertTo-SecureString -String "1234" -Force -AsPlainText)
+
+#if didnt work, import then fix
+Import-Certificate -CertStoreLocation Cert:\LocalMachine\My -FilePath <Certfile>
+Get-ChildItem Cert:\LocalMachine\My | Format-List Subject, Thumbprint, HasPrivateKey
+certutil -repairstore my <Thumbprint>
