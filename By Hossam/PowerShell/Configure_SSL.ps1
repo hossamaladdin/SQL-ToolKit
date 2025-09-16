8c5f6695d4222f8575ca90d6f95d021ac3e9e24f
hossam.alaa <hossam.alaaddin@gmail.com>
2025-09-16 14:46:04 +0300

    fixing certificate script

--- By Hossam/PowerShell/Configure_SSL.ps1
+++ By Hossam/PowerShell/Configure_SSL.ps1
@@ -2,7 +2,7 @@
 $CN = "$($env:COMPUTERNAME).$((Get-WmiObject Win32_ComputerSystem).Domain)"
 $infContent = @"
 [NewRequest]
-Subject = "CN=$CN"
+Subject = "CN=$CN , O=Mobily, OU=Information Technology, L=Riyadh, S=Riyadh, C=SA"
 KeyLength = 2048
 Exportable = TRUE
 "@
@@ -16,7 +16,19 @@ Write-Output "CSR saved to: $ReqPath"
 #try
 certreq -accept <Certfile>
 
+#if didn't work then need to add root and intermediate certs first
+certutil -addstore Root <rootCAfile>
+certutil -addstore CA <intermediateCAfile>
+certreq -accept <Certfile>
+
 #if didnt work, import then fix
 Import-Certificate -CertStoreLocation Cert:\LocalMachine\My -FilePath <Certfile>
 Get-ChildItem Cert:\LocalMachine\My | Format-List Subject, Thumbprint, HasPrivateKey
 certutil -repairstore my <Thumbprint>
+
+#if still didnt work, export and re-import
+$cert = Get-ChildItem Cert:\CurrentUser\My | Where-Object { $_.Subject -like "*O=Mobily*" }
+$cert | Export-PfxCertificate -FilePath "$env:USERPROFILE\Documents\MobilyCert.pfx" -Password (ConvertTo-SecureString -String "1234" -Force -AsPlainText)
+
+
+Import-PfxCertificate -FilePath "$env:USERPROFILE\Documents\MobilyCert.pfx" -CertStoreLocation Cert:\LocalMachine\My -Password (ConvertTo-SecureString -String "1234" -Force -AsPlainText)
